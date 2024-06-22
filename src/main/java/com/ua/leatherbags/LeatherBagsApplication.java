916997b9260f6a4ee8d32d147087cb80cff76f41
deltaonlyone <deltaonlyone@gmail.com>
2024-06-22 17:09:12 +0300

    init

---
+++ src/main/java/com/ua/leatherbags/LeatherBagsApplication.java
@@ -0,0 +1,13 @@
+package com.ua.leatherbags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeatherBagsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeatherBagsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/leatherbags/LeatherBagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.leatherbags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeatherBagsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
