01d10c1696b42cf612257fdaebf255a4715c3149
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2024-07-28 16:40:59 +0300

    Added OpenAPI documentation

--- src/main/java/com/ua/leatherbags/controller/BagController.java
+++ src/main/java/com/ua/leatherbags/controller/BagController.java
@@ -1,26 +1,100 @@
 package com.ua.leatherbags.controller;
 
-import com.ua.leatherbags.dao.SliceWrapper;
+import com.ua.leatherbags.dao.PageWrapper;
 import com.ua.leatherbags.data.Bag;
 import com.ua.leatherbags.data.OrderStatus;
 import com.ua.leatherbags.service.BagService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bags")
 @RequiredArgsConstructor
+@Tag(
+		name = "Bag",
+		description = "Bag Order info"
+)
 public class BagController {
 	private final BagService bagService;
 
 	@GetMapping
-	public SliceWrapper<Bag> getBags(@RequestParam byte status,
-									 @RequestParam int page,
-									 @RequestParam int size) {
+	@Operation(
+			description = "All bag orders info with pagination and filtering by status. All status codes can be retrieved using /statuses",
+			summary = "All bag orders info",
+			parameters = {
+					@Parameter(
+							in = ParameterIn.QUERY,
+							name = "status",
+							description = "Status value of orders to be retrieved",
+							example = "1"),
+					@Parameter(
+							in = ParameterIn.QUERY,
+							name = "page",
+							description = "Page number, starts from 1",
+							example = "1"),
+					@Parameter(
+							in = ParameterIn.QUERY,
+							name = "size",
+							description = "Size of a page",
+							example = "15")
+			},
+			responses = {
+					@ApiResponse(
+							description = "Page of bag info",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = PageWrapper.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							ref = "#/components/responses/403Response"
+					)
+			}
+	)
+	public PageWrapper<Bag> getBags(@RequestParam byte status,
+									@RequestParam int page,
+									@RequestParam int size) {
 		return bagService.findBagsByStatus(status, page, size);
 	}
 
 	@PostMapping
+	@Operation(
+			description = "Request a bag",
+			summary = "Bag order request",
+			requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+					description = "Order's bag and customer info",
+					content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = Bag.class)
+					)
+			),
+			responses = {
+					@ApiResponse(
+							description = "Created Order",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Bag.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							ref = "#/components/responses/403Response"
+					)
+			}
+	)
+	@SecurityRequirements()
 	public Bag addBag(@RequestBody Bag bag) {
 		bag = bagService.saveBag(bag);
 
@@ -28,16 +102,84 @@ public class BagController {
 	}
 
 	@PutMapping("/proceed")
+	@Operation(
+			description = "Proceed a certain order by id",
+			summary = "Proceed order",
+			requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+					description = "Id of a request to proceed",
+					content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = Integer.class, example = "1")
+					)
+			),
+			responses = {
+					@ApiResponse(
+							description = "Proceeded order",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Bag.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							ref = "#/components/responses/403Response"
+					)
+			}
+	)
 	public Bag proceedBag(@RequestBody long id) {
 		return bagService.proceedOrder(id);
 	}
 
 	@PutMapping("/cancel")
+	@Operation(
+			description = "Cancel a certain order by id",
+			summary = "Cancel order",
+			requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+					description = "Id of a request to canceled",
+					content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = Integer.class, example = "1")
+					)
+			),
+			responses = {
+					@ApiResponse(
+							description = "Canceled order",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Bag.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							ref = "#/components/responses/403Response"
+					)
+			}
+	)
 	public Bag cancelBag(@RequestBody long id) {
 		return bagService.cancelOrder(id);
 	}
 
 	@GetMapping("/statuses")
+	@Operation(
+			description = "All order statuses",
+			summary = "All order statuses",
+			responses = {
+					@ApiResponse(
+							description = "All order statuses",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = OrderStatus[].class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							ref = "#/components/responses/403Response"
+					)
+			}
+	)
 	public OrderStatus[] getOrderStatuses() {
 		return OrderStatus.values();
 	}
--- src/main/java/com/ua/leatherbags/data/Bag.java
+++ src/main/java/com/ua/leatherbags/data/Bag.java
@@ -1,6 +1,7 @@
 package com.ua.leatherbags.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,18 +18,24 @@ public class Bag {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
+	@Schema(accessMode = Schema.AccessMode.READ_ONLY,
+			example = "0")
 	private long id;
 
 	@Column(name = "first_name")
+	@Schema(example = "John")
 	private String firstName;
 
 	@Column(name = "last_name")
+	@Schema(example = "Doe")
 	private String lastName;
 
 	@Column(name = "middle_name")
+	@Schema(example = "")
 	private String middleName;
 
 	@Column(name = "phone_num")
+	@Schema(example = "+380000000000")
 	private String phoneNum;
 
 	@Column(name = "order_date")
@@ -38,24 +45,34 @@ public class Bag {
 	private LocalDateTime modifyDate;
 
 	@Column(name = "color")
+	@Schema(example = "Black")
 	private String color;
 
 	@Column(name = "size")
+	@Schema(example = "XL")
 	private String size;
 
 	@Column(name = "type")
+	@Schema(example = "Bag Type 1")
 	private String type;
 
+	@Column(name = "key_holder")
+	@Schema(example = "false")
+	private boolean keyHolder;
+
 	@Column(name = "city")
+	@Schema(example = "Lviv, Lvivska Obl.")
 	private String city;
 
 	@Column(name = "department")
-	private int department;
+	@Schema(example = "Department 54, Rusovyx St., 2")
+	private String department;
 
 	@Column(name = "status")
 	@JsonIgnore
 	private byte status;
 
+	@Schema(example = "Processing")
 	public String getStatusName() {
 		return OrderStatus.getName(status);
 	}
